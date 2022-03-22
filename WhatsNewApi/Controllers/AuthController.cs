3f62379e2f73579a665a82c8c460f0c9640ec71d
Samuel Montambault <mont.samuel@outlook.com>
2022-03-22 12:44:36 -0400

    Setup test (#3)
    * Login implemented and initial extensions for DI
    
    * Create user with role and refresh token
    
    * Missing lines in auth controller
    
    * Removed unused usings
    
    * Change namespaces
    
    * Create main-workflow.yaml
    
    * Update main-workflow.yaml
    
    * add logging and test project
    
    * rename test proect
    
    * add first tests and documentation
    
    * remove entities
    
    * fix tests
    
    * fix tests
    
    Co-authored-by: Jonathan Duval-Venne <duvalvenne.jonathan@gmail.com>

--- WhatsNewApi/Controllers/AuthController.cs
+++ WhatsNewApi/Controllers/AuthController.cs
@@ -21,20 +21,35 @@ public class AuthController : ControllerBase
         _firebaseService = firebaseService;
     }
 
+    /// <summary>
+    /// An endpoint to login in Firebase will return 200 OK if the login was successful.
+    /// </summary>
+    /// <param name="userCrendentials">Credential to login with used an email, password</param>
+    /// <returns>Return Unauthorized 401 or OK 200 depending on if the login was successful</returns>
     [AllowAnonymous]
     [HttpPost]
     public async Task<IActionResult> Login([FromBody] UserLoginCrendentialsDTO userCrendentials)
     {
-        if(!string.IsNullOrEmpty(userCrendentials.Email) && !string.IsNullOrEmpty(userCrendentials.Password))
+        try
         {
-            var user = await _authService.Authenticate(userCrendentials.Email, userCrendentials.Password);
-            return Ok(user);
+            if (!string.IsNullOrEmpty(userCrendentials.Email) && !string.IsNullOrEmpty(userCrendentials.Password))
+            {
+                var user = await _authService.Authenticate(userCrendentials.Email, userCrendentials.Password);
+                return Ok(user);
+            }
+            return Unauthorized();
+        } 
+        catch (Exception ex)
+        {
+            return Unauthorized(ex.Message);
         }
-            
-        return BadRequest();
     }
-
-    // Only admins should be able to create accounts for others
+    
+    /// <summary>
+    /// Register allows tyo create new account in the firebase project. Only admins should be able to create accounts for others
+    /// </summary>
+    /// <param name="userDto">Should have an email, a password and it's confirmation and the member role</param>
+    /// <returns>A 200 OK if the registering has been successful or 400 if the user couldn't be created</returns>
     [HttpPost("register")]
     [Authorize(Roles = "Administrator")]
     public async Task<IActionResult> Register([FromBody] UserCreationDTO userDto)
@@ -55,6 +70,11 @@ public class AuthController : ControllerBase
         }
     }
 
+    /// <summary>
+    /// Refresh the JWT token provided 
+    /// </summary>
+    /// <param name="userDto"></param>
+    /// <returns>the user of the requested refresh in a 200 OK, if an error occurs 400 Bad request</returns>
     [HttpPost("refresh")]
     [AllowAnonymous]
     public async Task<IActionResult> RefreshAuth([FromBody] UserDTO userDto)
---
+++ test/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Moq;
+global using System.Threading.Tasks;
+global using WhatsNewApi.Controllers;
+global using WhatsNewApi.Models.DTOs;
+global using WhatsNewApi.Services.Abstractions;
+global using Xunit;
