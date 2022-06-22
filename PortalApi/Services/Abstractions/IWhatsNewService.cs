0af8f97413c6ad0a5ac5e94f58fb91a6270d8223
Jonathan Duval-Venne <45638733+JonathanDuvalV@users.noreply.github.com>
2022-06-22 17:58:41 -0400

    Update PortalApi/Services/Abstractions/IWhatsNewService.cs
    Co-authored-by: Samuel Montambault <mont.samuel@outlook.com>

--- PortalApi/Services/Abstractions/IWhatsNewService.cs
+++ PortalApi/Services/Abstractions/IWhatsNewService.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using WhatsNewApi.Models.FirestoreModels;
 
-namespace WhatsNewApi.Services.Abstractions
+namespace WhatsNewApi.Services.Abstractions;
+
+public interface IWhatsNewService
 {
-	public interface IWhatsNewService
-    {
-        public Task<IEnumerable<WhatsNew>> GetAllWhatsNews(string projectId);
-        public Task<WhatsNew> GetWhatsNew(string projectId, string version);
-        public Task<IEnumerable<WhatsNew>> GetWhatsNewsInRange(string projectId, string from, string to);
-        public Task CreateWhatsNew(string projectId, string version, IEnumerable<WhatsNewPage> pages);
-        public Task UpdateWhatsNew(string id, WhatsNew whatsNew);
-        public Task DeleteWhatsNew(string id);
-    }
+    public Task<IEnumerable<WhatsNew>> GetAllWhatsNews(string projectId);
+    public Task<WhatsNew> GetWhatsNew(string projectId, string version);
+    public Task<IEnumerable<WhatsNew>> GetWhatsNewsInRange(string projectId, string from, string to);
+    public Task CreateWhatsNew(string projectId, string version, IEnumerable<WhatsNewPage> pages);
+    public Task UpdateWhatsNew(string id, WhatsNew whatsNew);
+    public Task DeleteWhatsNew(string id);
 }
 
