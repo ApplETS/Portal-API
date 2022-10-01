3753647cbe8b02d85c2cffb69684083070a8a173
Samuel Montambault <mont.samuel@outlook.com>
2022-09-30 22:30:52 -0400

    swagger for prod

--- PortalApi/Program.cs
+++ PortalApi/Program.cs
@@ -67,12 +67,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthentication();
 
